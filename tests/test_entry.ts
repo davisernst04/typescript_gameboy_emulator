9718b9f1232e1d99691ef237d349b7b8614811d7
Davis Ernst <davis@example.com>
2026-03-13 23:07:51 -0600

    Update test files and dependencies

--- tests/test_entry.ts
+++ tests/test_entry.ts
@@ -1,1 +1,5 @@
-export { cpu } from '../src/cpu'; export { gpu } from '../src/gpu'; export { mmu } from '../src/mmu'; export { log } from '../src/log'; export { emulator } from '../src/main';
+export { cpu } from '../src/emulator/cpu';
+export { gpu } from '../src/emulator/gpu';
+export { mmu } from '../src/emulator/mmu';
+export { log } from '../src/emulator/log';
+export { emulator } from '../src/emulator/main';
